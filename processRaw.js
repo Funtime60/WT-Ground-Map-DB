fc1cb68f5bb7d708c30e0a5ecd2ae2a62d97614c
Funtime60 <24595923+Funtime60@users.noreply.github.com>
2025-07-11 20:54:52 -0600

    finish functionality of process script

--- processRaw.js
+++ processRaw.js
@@ -7,6 +7,7 @@ logger.level = "debug";
 
 const filePath = 'mapFingerprintRaw.json';
 const workingDir = 'mapFingerprints/';
+const outputDBPath = 'mapFingerprintsDB.json';
 
 async function main() {
 	let data;
@@ -33,7 +34,7 @@ async function main() {
 			outputData = data[mapName].reduce((accumulator, currentValue, index) => {
 				currentValue.map_obj.forEach((map_obj, iter_index) => {
 					const sameTeam = index == 0 || map_obj.team == accumulator.map_obj?.[0]?.team;
-					if(!accumulator.map_obj.some((obj) => obj.type == map_obj.type && obj?.x == map_obj?.x && obj?.y == map_obj?.y && obj?.sx == map_obj?.sx && obj?.sy == map_obj?.sy && obj?.ex == map_obj?.ex && obj?.ey == map_obj?.ey && obj?.dx == map_obj?.dx && obj?.dy == map_obj?.dy)) {
+					if(!accumulator.map_obj.some((obj) => isSameMap_Obj(obj, map_obj))) {
 						if(!sameTeam && map_obj.team) {
 							map_obj.team = map_obj.team == "friendly" ? "enemy" : "friendly";			// If the team is not the same, switch it.
 						}
@@ -43,6 +44,9 @@ async function main() {
 				return accumulator;
 			}, {mission: data[mapName][0].mission, map_obj: [], mapInfo: data[mapName][0].mapInfo});	// If the map data is an array, push it to the outputData
 		}
+		outputData.map_obj.forEach((map_obj) => {											// Iterate over the map objects in the outputData
+			map_obj.team = map_obj?.team?.replace(/friendly|enemy/, (match) => match == "friendly" ? "team1" : "team2");	// Capitalize the team name
+		});
 		try {
 			fs.mkdirSync(workingDir);
 		}catch(err) {
@@ -53,10 +57,35 @@ async function main() {
 		}
 		fs.writeFileSync(`${workingDir}${sanitize(mapName)}.json`, JSON.stringify(outputData, null, 2), 'utf8');
 	});
-	
+	const outputData = {x:{}, y:{}};
+	fs.readdirSync(workingDir, {withFileTypes: true}).forEach((file) => {	// Iterate over the files in the working directory
+		data = JSON.parse(fs.readFileSync(`${file.parentPath}${file.name}`, 'utf8')).map_obj;
+		data.forEach((map_obj) => {											// Iterate over the map objects in the file
+			const xCoords = ['',...'abcdefghijklmnopqrstuvwxyz'].map((char) => map_obj[`${char}x`]).filter((value) => value != undefined);
+			const yCoords = ['',...'abcdefghijklmnopqrstuvwxyz'].map((char) => map_obj[`${char}y`]).filter((value) => value != undefined);
+			xCoords.forEach((_, index) => {
+				map_obj.map_name = [file.name.replace(/\.json$/, '')];	// Set the map name to the file name without the .json extension
+				outputData.x[xCoords[index]] = {};
+				outputData.y[yCoords[index]] = {};
+				if(outputData.x[xCoords[index]][yCoords[index]]) {
+					const existingIndex = outputData.x[xCoords[index]][yCoords[index]].findIndex((obj) => isSameMap_Obj(obj, map_obj));
+					if(existingIndex == -1) {
+						outputData.x[xCoords[index]][yCoords[index]][existingIndex].map_name.push(map_obj.map_name[0]);		// If the map object is not already in the array, add it
+					}else {
+						outputData.x[xCoords[index]][yCoords[index]].push(map_obj);
+					}
+				}
+			});
+		});
+	});
+	fs.writeFileSync(outputDBPath, JSON.stringify(outputData, null, 2), 'utf8');
 }
 
 main().then((returnCode) => process.exit(returnCode));
+
+function isSameMap_Obj(obj, map_obj) {
+	return obj.type == map_obj.type && obj?.x == map_obj?.x && obj?.y == map_obj?.y && obj?.sx == map_obj?.sx && obj?.sy == map_obj?.sy && obj?.ex == map_obj?.ex && obj?.ey == map_obj?.ey && obj?.dx == map_obj?.dx && obj?.dy == map_obj?.dy;
+}
 
 function getLine() {
 	return Number(new Error().stack?.split(/\n/g)?.[2].split(/:/)?.at(-2));
